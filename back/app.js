fb595ddca1b6b10db77a920b570725992dd7b08e
Christian TÃéllez <ch.tellez@hotmail.com>
2021-12-04 01:00:59 -0500

    configurando back inicial

--- back/app.js
+++ back/app.js
@@ -1,34 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyparser = require('body-parser');
+const logger = require('morgan');
 
-var app = express();
+// Immportamos la configuracion de la base de datos
+const database = require('./config/database');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger('tiny'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', require('./routes/routes'));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Mongo connection
+database.mongoConnect();
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

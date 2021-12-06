9d8190fb335ce0e3ff340747e7001c869489f9d5
DiDi7T <dtnirvana@gmail.com>
2021-12-05 22:57:59 -0500

    uniendo otra vez

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-<<<<<<< HEAD
 import Axios from 'axios'
-=======
 import './styles/hideScrollBars.css';
->>>>>>> 188bfc24ea9fbc9e865cd7b1f7031964b1741426
 
 Axios.defaults.baseURL='http://localhost:4000'
 ReactDOM.render(
